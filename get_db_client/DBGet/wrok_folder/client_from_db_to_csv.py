25cc00069d9161725dc182808ae89dfba2820cdf
Yaroslav Balaban <yarchick.balaban@gmail.com>
2021-11-13 21:04:40 +0200

    Fixed class methods

--- get_db_client/DBGet/wrok_folder/client_from_db_to_csv.py
+++ get_db_client/DBGet/wrok_folder/client_from_db_to_csv.py
@@ -1,7 +1,12 @@
 import pymysql.cursors
 import csv
+from datetime import datetime as dt
 
-class getConnect:
+
+# Request string to retrieve from the database name, mail, phone number from the database
+QUERY = 'SELECT `firstname`, `lastname`, `email`, `telephone` FROM `oc_customer`'
+
+class DBWriteToFile:
 
     def __init__(self, host, user, password, db):
         self.host = host
@@ -9,7 +14,12 @@ class getConnect:
         self.password = password
         self.db = db
 
-    def connections(self):
+    def start(self):
+        get_connect = self.get_connections_to_db(self.host, self.user, self.password, self.db)
+        extract_data = self.get_from_db(get_connect)
+        write_data_to_file = self.write_to_file(extract_data)
+
+    def get_connections_to_db(self, host, user, password, db):
         connection = pymysql.connect(
             host=self.host,
             user=self.user,
@@ -18,28 +28,28 @@ class getConnect:
             charset='utf8',
             cursorclass=pymysql.cursors.DictCursor,
         )
-        self.get_query(connection)
+        return connection
 
-    def get_query(self, connection):
+    def get_from_db(self, connection):
         with connection as conn:
             cur = conn.cursor()
-            query = 'SELECT `firstname`, `lastname`, `email`, `telephone` FROM `oc_customer`'
+            query = QUERY
             cur.execute(query)
             rows = cur.fetchall()
+            print(type(rows))
             result = []
-            cont = 0
             for i in rows:
                 i['Имя'] = i.pop('firstname')
                 i['Фамилия'] = i.pop('lastname')
                 i['Почта'] = i.pop('email')
                 i['Телефон'] = i.pop('telephone')
                 result.append(i)
-                cont += 1
-            self.write_to_file(result)
+            return result
 
     def write_to_file(self, result):
+        __now = dt.now()
         csv_column_order = list(result[0].keys())
-        with open('exported.csv', 'w', newline='') as csvfile:
+        with open(f'exported_{__now.strftime("%H-%M-%S")}.csv', 'a', newline='') as csvfile:
             myCsvWriter = csv.DictWriter(csvfile, delimiter=',',
                                          quotechar='"',
                                          fieldnames=csv_column_order)
