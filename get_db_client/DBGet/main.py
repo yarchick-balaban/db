bdc907c2dd7ed153b609708c785636c16cf4cd96
Yaroslav Balaban <yarchick.balaban@gmail.com>
2021-11-13 21:04:40 +0200

    Fixed class methods

--- get_db_client/DBGet/main.py
+++ get_db_client/DBGet/main.py
@@ -1,4 +1,4 @@
-from get_db_client.DBGet.wrok_folder.client_from_db_to_csv import getConnect
+from get_db_client.DBGet.wrok_folder.client_from_db_to_csv import getDBWriteToFile
 import configparser
 
 #The folder should contain a configuration file in a standard form:
@@ -13,9 +13,9 @@ config = configparser.ConfigParser()
 config.read(CONFIG_FILE_PATH, encoding='utf-8-sig')
 
 if __name__ == "__main__":
-    get_db = getConnect(
+    write_from_db_to_file = getDBWriteToFile(
         host=config.get('mysql', 'host'),
         user=config.get('mysql', 'user'),
         password=config.get('mysql', 'password'),
         db=config.get('mysql', 'db'))
-    get_db.connections()
+    write_from_db_to_file.start()
